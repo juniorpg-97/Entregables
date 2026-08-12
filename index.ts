48c6a60adf057ce663d79d18ff3bb9579b1dd38d
unknown <portohector97@gmail.com>
2026-08-11 19:00:12 -0500

    feat: menú interactivo con ciclo y manejo de arreglo

--- index.ts
+++ index.ts
@@ -6,15 +6,57 @@ const rl = readline.createInterface({ input, output });
 
 // ✍️ Escribe tu código aquí 👇
 
-const systemName: string = "Android";
-const version: number = 10.0;
-const userName: string = "Junior";
+//const systemName: string = "Android";
+//const version: number = 10.0;
+//const userName: string = "Junior";cls
 
-const mensaje: string = systemName + "  v" + version + "¡Bienvenido, " + userName + "!";
-console.log(mensaje);
 
+//const mensaje: string = systemName + "  v" + version + "¡Bienvenido, " + userName + "!";
+//console.log(mensaje);
 
 // Comentario: cuando ejecuto ts-node index.ts para verificar si mi codigo funciona, me sale error, creo que el error es en la compatibilidad de la versión de TypeScript y Node
+
+
+//const answer = await rl.question("¿Cuál es tu nombre? ");
+//console.log(`Hola, ${answer}!`);
+
+
+let tareas: string[] = [];
+let opcion = "";
+
+while (opcion !== "4") {
+
+    console.log("1 Agregar tarea");
+    console.log("2 Eliminar ultima tarea");
+    console.log("3 Lista de tareas");
+    console.log("4 Salir");
+
+    opcion = await rl.question("Ingrese una opción ");
+
+    switch (opcion) {
+
+        case "1":
+            const tarea = await rl.question("Ingresa una tarea: ");
+            tareas.push(tarea);
+            console.log("Tarea agregada");
+            break;
+
+        case "2":
+            const eliminada = tareas.pop();
+            console.log("Tarea " + eliminada + " eliminada");
+            break;
+
+        case "3":
+            for (let index = 0; index < tareas.length; index++) {
+                console.log(tareas[index]);
+            }
+            break;
+
+        case "4":
+            console.log("Saliendo");
+            break;
+    }
+}
 
 
 // 🚫 No eliminar las líneas de abajo ⬇️
